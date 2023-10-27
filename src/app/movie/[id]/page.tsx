87eebb7a45734c8b33579411fd0c0445bc4d436a
Paritosh Yadav <paritosh@indiagold.co>
2023-10-28 02:56:35 +0400

    commit types

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -3,14 +3,20 @@ import React from "react";
 
 const API_KEY = process.env.API_KEY;
 
-const getMovie = async (movieId) => {
+const getMovie = async (movieId: number) => {
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`
   );
   return await res.json();
 };
 
-const MoviePage = async ({ params }) => {
+const MoviePage = async ({
+  params,
+}: {
+  params: {
+    id: number;
+  };
+}) => {
   const movieId = params.id;
   const movie = await getMovie(movieId);
   return (
